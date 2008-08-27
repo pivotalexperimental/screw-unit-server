78288e12487bbb3c810c6a826e19a4210ba0b04d
Brian Takita <grockit@Macpro03-desktop.(none)>
2008-08-27 16:09:10 -0700

    Functional spec passes.

--- spec/functional/functional_spec.rb
+++ spec/functional/functional_spec.rb
@@ -1,14 +1,25 @@
 require File.expand_path("#{File.dirname(__FILE__)}/functional_spec_helper")
 
 describe "ScrewUnit" do
+  attr_reader :stdout, :request
+  before do
+    @stdout = StringIO.new
+    ScrewUnit::Client.const_set(:STDOUT, stdout)
+    @request = "http request"
+  end
+
+  after do
+    ScrewUnit::Client.__send__(:remove_const, :STDOUT)
+  end
+
   it "runs a full passing Suite" do
-    mock(ScrewUnit::Client).puts("SUCCESS")
     ScrewUnit::Client.run(:spec_url => "#{root_url}/specs/foo/passing_spec")
+    stdout.string.strip.should == "SUCCESS"
   end
 
   it "runs a full failing Suite" do
-    mock(ScrewUnit::Client).puts("FAILURE")
-    mock(ScrewUnit::Client).puts('expected true to equal false')
     ScrewUnit::Client.run(:spec_url => "#{root_url}/specs/foo/failing_spec")
+    stdout.string.strip.should include("FAILURE")
+    stdout.string.strip.should include("expected true to equal false")
   end
 end
