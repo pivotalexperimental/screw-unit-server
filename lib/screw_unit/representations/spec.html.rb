d68e6c6383996c0c4a3baf07d5ec2650ea79239c
Brian Takita <brian.takita@pivotallabs.com>
2009-05-11 23:02:07 -0700

    Better error message when running with Selenium.

--- lib/screw_unit/representations/spec.html.rb
+++ lib/screw_unit/representations/spec.html.rb
@@ -59,10 +59,19 @@ module ScrewUnit
           (function($) {
             var ajax = $.ajax;
             $(Screw).bind('after', function() {
-              var error_text = $(".error").map(function(i, element) {
-                var error_parts = [];
-                var parents = $(element).parents("li.describe");
-                return $(parents[parents.length - 1]).text();
+              var error_text = $(".error").map(function(i, error_element) {
+                var element = $(error_element);
+
+                var parent_descriptions = element.parents("li.describe");
+                var parent_description_text = [];
+
+                for(var i=parent_descriptions.length-1; i >= 0; i--) {
+                  parent_description_text.push($(parent_descriptions[i]).find("h1").text());
+                }
+
+                var it_text = element.parents("li.it").find("h2").text();
+
+                return parent_description_text.join(" ") + " " + it_text + ": " + element.text();
               }).get().join("\\n");
 
               ajax({
