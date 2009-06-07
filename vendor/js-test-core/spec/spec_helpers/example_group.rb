5cdb6a755059659cfc070899bcd69a36abaff31b
Brian Takita <brian.takita@gmail.com>
2009-06-07 04:36:41 -0700

    Braid: Updated mirror 'vendor/js-test-core' to '2d2cfd0'

--- vendor/js-test-core/spec/spec_helpers/example_group.rb
+++ vendor/js-test-core/spec/spec_helpers/example_group.rb
@@ -1,5 +1,6 @@
 class Spec::ExampleGroup
   include Rack::Test::Methods
+  include BeHttp
   attr_reader :core_path, :spec_root_path, :implementation_root_path, :public_path, :server, :connection
   before(:all) do
     dir = File.dirname(__FILE__)
@@ -19,35 +20,5 @@ class Spec::ExampleGroup
 
   def app
     Sinatra::Application
-  end
-
-  def be_http(status, headers, body)
-    SimpleMatcher.new(nil) do |given, matcher|
-      description = (<<-DESC).gsub(/^ +/, "")
-      be an http of
-      expected status: #{status.inspect}
-      actual status  : #{given.status.inspect}
-
-      expected headers containing: #{headers.inspect}
-      actual headers             : #{given.headers.inspect}
-
-      expected body containing: #{body.inspect}
-      actual body             : #{given.body.inspect}
-      DESC
-      matcher.failure_message = description
-      matcher.negative_failure_message = "not #{description}"
-
-      passed = true
-      unless given.status == status
-        passed = false
-      end
-      unless headers.all?{|k, v| given.headers[k] == headers[k]}
-        passed = false
-      end
-      unless body.is_a?(Regexp) ? given.body =~ body : given.body.include?(body)
-        passed = false
-      end
-      passed
-    end
   end
 end
--- vendor/js-test-core/spec/unit/unit_spec_helper.rb
+++ vendor/js-test-core/spec/unit/unit_spec_helper.rb
@@ -10,6 +10,7 @@ $LOAD_PATH.unshift File.expand_path("#{LIBRARY_ROOT_DIR}/lib")
 require "js_test_core"
 require "nokogiri"
 require "guid"
+require "#{LIBRARY_ROOT_DIR}/spec/spec_helpers/be_http"
 require "#{LIBRARY_ROOT_DIR}/spec/spec_helpers/example_group"
 require "#{LIBRARY_ROOT_DIR}/spec/spec_helpers/fake_selenium_driver"
 require "#{LIBRARY_ROOT_DIR}/spec/spec_helpers/show_test_exceptions"
