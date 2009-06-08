ff153f7b873de0c404d596c6d652e19358ebcc37
Brian Takita <brian.takita@gmail.com>
2009-06-08 01:17:04 -0700

    Removed puts.

--- vendor/js-test-core/lib/js_test_core/resources/web_root.rb
+++ vendor/js-test-core/lib/js_test_core/resources/web_root.rb
@@ -4,7 +4,6 @@ module JsTestCore
       map "/"
       
       get("") do
-        puts "#{__FILE__}:#{__LINE__}"
         [200, {}, "<html><head></head><body>Welcome to the Js Test Server. Click the following link to run you <a href=/specs>spec suite</a>.</body></html>"]
       end
     end
