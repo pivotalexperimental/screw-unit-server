7e237ab4aa4c22c04046eacb4ebbab9c149d175e
Brian Takita <brian.takita@pivotallabs.com>
2009-06-08 11:59:12 -0700

    Requiring thin in ScrewUnit::Server.

--- lib/screw_unit/server.rb
+++ lib/screw_unit/server.rb
@@ -1,6 +1,7 @@
 module ScrewUnit
   class Server
     def self.start
+      require "thin"
       Thin::Runner.new([
         "--port", "8080",
         "--rackup", File.expand_path("#{File.dirname(__FILE__)}/../../standalone.ru"),
