97d1d3f2a9f30eac6754d8baa7efaf25cb1d7629
Brian Takita <brian.takita@gmail.com>
2009-06-17 22:14:24 -0700

    Braid: Updated mirror 'vendor/js-test-core' to '7310da9'

--- vendor/js-test-core/lib/js_test_core/representations/spec.html.rb
+++ vendor/js-test-core/lib/js_test_core/representations/spec.html.rb
@@ -1,6 +1,18 @@
 module JsTestCore
   module Representations
     class Spec < Page
+      class << self
+        def project_js_files
+          @project_js_files ||= []
+        end
+        attr_writer :project_js_files
+
+        def project_css_files
+          @project_css_files ||= []
+        end
+        attr_writer :project_css_files
+      end
+
       needs :spec_files, :session_id
       protected
       def title_text
@@ -8,6 +20,8 @@ module JsTestCore
       end
 
       def head_content
+        project_js_files
+        project_css_files
         spec_script_elements
       end
 
@@ -20,6 +34,18 @@ module JsTestCore
 
       def script_to_set_window_session_id
         script "window._session_id = '#{session_id}';", :type => "text/javascript"
+      end
+
+      def project_js_files
+        self.class.project_js_files.each do |file|
+          script :src => file, :type => "text/javascript"
+        end
+      end
+
+      def project_css_files
+        self.class.project_css_files.each do |file|
+          link :href => file, :type => "text/css", :media => "screen", :rel => "stylesheet"
+        end
       end
       
       def body_content
--- vendor/js-test-core/spec/unit/js_test_core/resources/spec_file_spec.rb
+++ vendor/js-test-core/spec/unit/js_test_core/resources/spec_file_spec.rb
@@ -3,6 +3,34 @@ require File.expand_path("#{File.dirname(__FILE__)}/../../unit_spec_helper")
 module JsTestCore
   module Resources
     describe SpecFile do
+      describe "Configuration" do
+        attr_reader :doc
+        before do
+          JsTestCore::Representations::Spec.project_js_files += ["/javascripts/test_file_1.js", "/javascripts/test_file_2.js"]
+          JsTestCore::Representations::Spec.project_css_files += ["/stylesheets/test_file_1.css", "/stylesheets/test_file_2.css"]
+
+          response = get(SpecFile.path("/failing_spec"))
+          response.should be_http( 200, {}, "" )
+
+          @doc = Nokogiri::HTML(response.body)
+        end
+
+        after do
+          JsTestCore::Representations::Spec.project_js_files.clear
+          JsTestCore::Representations::Spec.project_css_files.clear
+        end
+
+        it "renders project js files" do
+          doc.at("script[@src='/javascripts/test_file_1.js']").should_not be_nil
+          doc.at("script[@src='/javascripts/test_file_2.js']").should_not be_nil
+        end
+
+        it "renders project css files" do
+          doc.at("link[@href='/stylesheets/test_file_1.css']").should_not be_nil
+          doc.at("link[@href='/stylesheets/test_file_2.css']").should_not be_nil
+        end
+      end
+
       describe "Files" do
         describe "GET /specs/failing_spec" do
           it "renders a suite only for failing_spec.js as text/html" do
