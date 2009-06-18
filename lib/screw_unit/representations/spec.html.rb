fdb82449cfc88b2d2850dca49f3d7a458aebf235
Brian Takita <brian.takita@gmail.com>
2009-06-17 22:09:34 -0700

    - Added ScrewUnit::Representations::Spec.project_js_files, .project_css_files, and .jquery_js_file.

--- lib/screw_unit/representations/spec.html.rb
+++ lib/screw_unit/representations/spec.html.rb
@@ -1,6 +1,23 @@
 module ScrewUnit
   module Representations
     class Spec < JsTestCore::Representations::Spec
+      class << self
+        def project_js_files
+          @project_js_files ||= []
+        end
+        attr_writer :project_js_files
+
+        def project_css_files
+          @project_css_files ||= []
+        end
+        attr_writer :project_css_files
+
+        def jquery_js_file
+          @jquery_js_file ||= "/core/jquery-1.3.2.js"
+        end
+        attr_writer :jquery_js_file
+      end
+
       needs :spec_files
       def title_text
         "Screw Unit suite"
@@ -101,13 +118,19 @@ module ScrewUnit
       end
 
       def project_js_files
+        self.class.project_js_files.each do |file|
+          script :src => file, :type => "text/javascript"
+        end
       end
 
       def project_css_files
+        self.class.project_css_files.each do |file|
+          link :href => file, :type => "text/css", :media => "screen", :rel => "stylesheet"
+        end
       end
 
       def jquery_js_file
-        "/core/jquery-1.3.2.js"
+        self.class.jquery_js_file
       end
 
       def body_content
--- spec/unit/spec_file_spec.rb
+++ spec/unit/spec_file_spec.rb
@@ -6,28 +6,64 @@ module JsTestCore
       describe SpecFile do
         describe "File" do
           describe "GET /specs/failing_spec" do
-            attr_reader :html, :doc
-            before do
-              response = get(SpecFile.path("/failing_spec"))
-              response.should be_http(
-                200,
-                {},
-                ""
-              )
+            attr_reader :doc
 
-              @doc = Nokogiri::HTML(response.body)
+            context "when using the default .jquery_js_file" do
+              before do
+                ScrewUnit::Representations::Spec.project_js_files += ["/javascripts/test_file_1.js", "/javascripts/test_file_2.js"]
+                ScrewUnit::Representations::Spec.project_css_files += ["/stylesheets/test_file_1.css", "/stylesheets/test_file_2.css"]
+
+                response = get(SpecFile.path("/failing_spec"))
+                response.should be_http( 200, {}, "" )
+
+                @doc = Nokogiri::HTML(response.body)
+              end
+
+              after do
+                ScrewUnit::Representations::Spec.project_js_files.clear
+                ScrewUnit::Representations::Spec.project_css_files.clear
+              end
+
+              it "returns script tags for the test javascript file" do
+                doc.at("script[@src='/specs/failing_spec.js']").should_not be_nil
+              end
+
+              it "renders the screw unit template" do
+                doc.at("link[@href='/core/screw.css']").should_not be_nil
+                doc.at("script[@src='/core/screw.builder.js']").should_not be_nil
+                doc.at("script[@src='/core/screw.events.js']").should_not be_nil
+                doc.at("script[@src='/core/screw.behaviors.js']").should_not be_nil
+                doc.at("body/#screw_unit_content").should_not be_nil
+              end
+
+              it "renders project js files" do
+                doc.at("script[@src='/javascripts/test_file_1.js']").should_not be_nil
+                doc.at("script[@src='/javascripts/test_file_2.js']").should_not be_nil
+              end
+
+              it "renders project css files" do
+                doc.at("link[@href='/stylesheets/test_file_1.css']").should_not be_nil
+                doc.at("link[@href='/stylesheets/test_file_2.css']").should_not be_nil
+              end
             end
 
-            it "returns script tags for the test javascript file" do
-              doc.at("script[@src='/specs/failing_spec.js']").should_not be_nil
-            end
+            context "when using a custom .jquery_js_file" do
+              attr_reader :default_jquery_js_file
+              before do
+                @default_jquery_js_file = ScrewUnit::Representations::Spec.jquery_js_file
+                ScrewUnit::Representations::Spec.jquery_js_file = "/javascripts/jquery-6.6.6.js"
+                default_jquery_js_file.should_not == ScrewUnit::Representations::Spec.jquery_js_file
 
-            it "returns the screw unit template" do
-              doc.at("link[@href='/core/screw.css']").should_not be_nil
-              doc.at("script[@src='/core/screw.builder.js']").should_not be_nil
-              doc.at("script[@src='/core/screw.events.js']").should_not be_nil
-              doc.at("script[@src='/core/screw.behaviors.js']").should_not be_nil
-              doc.at("body/#screw_unit_content").should_not be_nil
+                response = get(SpecFile.path("/failing_spec"))
+                response.should be_http( 200, {}, "" )
+
+                @doc = Nokogiri::HTML(response.body)
+              end
+
+              it "renders the custom jquery file" do
+                doc.at("script[@src='/javascripts/jquery-6.6.6.js']").should_not be_nil
+                doc.at("script[@src='#{default_jquery_js_file}']").should be_nil
+              end
             end
           end
         end
